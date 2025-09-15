53d0a31c460e73412e4e46be5f492fdda3b9e05d
Agiless B B <155980818+Agiless@users.noreply.github.com>
2025-09-15 18:18:22 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',  # must be at top
-    'backend.middleware.ForceCORSHeadersMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'backend.middleware.ForceCORSHeadersMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
