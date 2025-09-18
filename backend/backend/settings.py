cf241d796d55dfe6c8e6520b0be3cf09f2ededf5
Agiless B B <155980818+Agiless@users.noreply.github.com>
2025-09-18 22:37:44 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -153,14 +153,14 @@ CHANNEL_LAYERS = {
     },
 }
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': (
+#         'rest_framework.authentication.TokenAuthentication',
+#     ),
+#     'DEFAULT_PERMISSION_CLASSES': (
+#         'rest_framework.permissions.IsAuthenticated',
+#     ),
+# }
 
 # ------------------------------
 # EXTERNAL KEYS
