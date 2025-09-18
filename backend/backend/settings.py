922cd92852cac5ce31088eebcdf774a4b6ee121c
Agiless B B <155980818+Agiless@users.noreply.github.com>
2025-09-18 20:51:33 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -153,6 +153,15 @@ CHANNEL_LAYERS = {
     },
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
 # ------------------------------
 # EXTERNAL KEYS
 # ------------------------------
